0d644e546704e240d71dd24e9dda39fb5d509db5
Szymek Radziszewski <szymon.radziszewski93@gmail.com>
2020-05-02 15:29:47 +0200

    bug fix

--- src/main/java/com/example/demo/api/controller/PostController.java
+++ src/main/java/com/example/demo/api/controller/PostController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     // for test purposes
-    @PreAuthorize("hasAnyRole()")
+    @PreAuthorize("hasAnyRole('ROLE_USER'.toString())")
     @GetMapping
     public String getPost(){ //for test purposes
         return "test";
