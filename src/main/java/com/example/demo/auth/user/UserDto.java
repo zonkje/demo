8a2d4a4e6a2333508b60b71ee518ec058ef881fb
Szymek Radziszewski <szymon.radziszewski93@gmail.com>
2020-05-08 00:09:31 +0200

    set builder for UserDto

--- src/main/java/com/example/demo/auth/user/UserDto.java
+++ src/main/java/com/example/demo/auth/user/UserDto.java
@@ -2,6 +2,7 @@ package com.example.demo.auth.user;
 
 import com.example.demo.auth.user.model.User;
 import com.example.demo.auth.user.model.UserRole;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,11 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Collections;
 
-@Builder
+
 @Data
-@NoArgsConstructor
+@NoArgsConstructor(force = true)
+@Builder
+@AllArgsConstructor
 public class UserDto {
 
     private Long id;
