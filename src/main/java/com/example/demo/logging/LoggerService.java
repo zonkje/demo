379bc72dc24d99fc67c3eef9e01d53f9b54876bc
Szymek Radziszewski <szymon.radziszewski93@gmail.com>
2021-01-18 23:33:41 +0100

    add logging config and method for logging intercepted request

--- src/main/java/com/example/demo/logging/LoggerService.java
+++ src/main/java/com/example/demo/logging/LoggerService.java
@@ -4,17 +4,59 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
 @Service
 public class LoggerService {
 
     private static final Logger LOG = LoggerFactory.getLogger("RestLogger");
 
-    public void request() {
+    public void request(HttpServletRequest request) {
         StringBuilder requestLogMessage = new StringBuilder();
+
+        requestLogMessage.append("\n*** REQUEST ************************************************************************\n")
+                .append("* IP      : ").append(request.getRemoteAddr()).append("\n")
+                .append("* URI     : ").append(request.getRequestURI()).append("\n")
+                .append("* METHOD  : ").append(request.getMethod()).append("\n")
+                .append("* QUERY   : ").append(request.getQueryString() != null ? request.getQueryString() : "<none>").append("\n")
+                .append("* HEADERS : ").append(getHeaders(request)).append("\n")
+                .append("************************************************************************************\n");
+
+        LOG.info(requestLogMessage.toString());
     }
 
     public void response() {
         StringBuilder responseLogMessage = new StringBuilder();
 
+        responseLogMessage.append("*** RESPONSE ************************************************/n")
+                .append("* REQUEST URI")
+                .append("* STATUS")
+                .append("* HEADERS")
+                .append("* BODY")
+                .append("\n************************************************************");
+
+        LOG.info(responseLogMessage.toString());
     }
+
+    private String getHeaders(HttpServletRequest request) {
+        StringBuilder headers = new StringBuilder();
+        request.getHeaderNames().asIterator().forEachRemaining(headerName -> headers.append("\n")
+                .append("\t- ")
+                .append(headerName.toUpperCase())
+                .append(" : ")
+                .append(request.getHeader(headerName))
+        );
+        return headers.toString();
+    }
+//private String getHeaders(HttpServletRequest request) {
+//    final var headers = new HashMap<String, String>();
+//    request.getHeaderNames().asIterator()
+//            .forEachRemaining(headerName -> headers.put(headerName, request.getHeader(headerName)));
+//    return headers.keySet().stream().map(key -> key + "=" + headers.get(key))
+//            .collect(Collectors.joining("\n", "{\n", "\n}"));
+//}
+
 }
---
+++ src/main/java/com/example/demo/logging/LoggingConfig.java
@@ -0,0 +1,41 @@
+package com.example.demo.logging;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+public class LoggingConfig implements WebMvcConfigurer {
+
+    private final LoggerService loggerService;
+
+    public LoggingConfig(LoggerService loggerService) {
+        this.loggerService = loggerService;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+registry.addInterceptor(new HandlerInterceptor() {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        loggerService.request(request);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("POST HANDLE **********************");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("AFTER COMPLETION ******************");
+    }
+});
+    }
+}
